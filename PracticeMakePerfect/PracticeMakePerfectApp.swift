1af0f905aa20d71b36d9ef283c5d28357e47fc2f
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-05-11 21:00:42 +0700

    Initial Commit

---
+++ PracticeMakePerfect/PracticeMakePerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeMakePerfectApp.swift
+//  PracticeMakePerfect
+//
+//  Created by Farhandika Zahrir Mufti guenia on 11/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PracticeMakePerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
